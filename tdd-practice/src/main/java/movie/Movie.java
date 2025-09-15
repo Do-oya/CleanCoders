fa8c434be96f119665a69e4f6e8448f0d8f6827b
Park JeongHyun <qkrwjdgus1441@naver.com>
2025-09-15 12:39:06 +0900

    feat: 1을 넣었을때 값이 1인지 검증하는 실패하는 테스트 작성

--- tdd-practice/src/main/java/movie/Movie.java
+++ tdd-practice/src/main/java/movie/Movie.java
@@ -4,4 +4,8 @@ public class Movie {
     public Integer averageRating() {
         return 0;
     }
+
+    public void rate(int rate) {
+
+    }
 }
--- tdd-practice/src/test/java/movie/MovieTest.java
+++ tdd-practice/src/test/java/movie/MovieTest.java
@@ -10,4 +10,11 @@ class MovieTest {
         Movie movie = new Movie();
         assertThat(movie.averageRating()).isEqualTo(0);
     }
+
+    @Test
+    void should_return_1_when_1_was_rated() {
+        Movie movie = new Movie();
+        movie.rate(1);
+        assertThat(movie.averageRating()).isEqualTo(1);
+    }
 }
