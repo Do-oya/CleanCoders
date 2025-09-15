205b3f4bcef08d7c53ae146aeb020ef0d06abae1
Park JeongHyun <qkrwjdgus1441@naver.com>
2025-09-15 18:05:05 +0900

    feat: 테스트 설정 추가

--- tyrant-client-tdd/build.gradle.kts
+++ tyrant-client-tdd/build.gradle.kts
@@ -12,9 +12,25 @@ repositories {
 dependencies {
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
+    testImplementation("org.assertj:assertj-core:3.24.2")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 }
 
 tasks.test {
     useJUnitPlatform()
+
+    // 테스트 결과를 콘솔에 출력
+    testLogging {
+        events("passed", "skipped", "failed")
+        exceptionFormat = org.gradle.api.tasks.testing.logging.TestExceptionFormat.FULL
+        showExceptions = true
+        showCauses = true
+        showStackTraces = true
+    }
+}
+
+// 자바 버전 설정
+java {
+    sourceCompatibility = JavaVersion.VERSION_17
+    targetCompatibility = JavaVersion.VERSION_17
 }
