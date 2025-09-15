60951e67bb6273150ebc6f49577d81e8f0735665
Park JeongHyun <qkrwjdgus1441@naver.com>
2025-09-15 12:46:43 +0900

    refactor: 중복되는 Movie 생성 필드 추출

--- tdd-practice/src/test/java/movie/MovieTest.java
+++ tdd-practice/src/test/java/movie/MovieTest.java
@@ -1,19 +1,25 @@
 package movie;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 class MovieTest {
+    private Movie movie;
+
+    @BeforeEach
+    void setUp() {
+        movie = new Movie();
+    }
+
     @Test
     void should_return_0_when_just_created() {
-        Movie movie = new Movie();
         assertThat(movie.averageRating()).isEqualTo(0);
     }
 
     @Test
     void should_return_1_when_1_was_rated() {
-        Movie movie = new Movie();
         movie.rate(1);
         assertThat(movie.averageRating()).isEqualTo(1);
     }
