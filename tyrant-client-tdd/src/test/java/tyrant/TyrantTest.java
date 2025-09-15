1037299f77611a25b8fe046f7ff678a574968fdb
Park JeongHyun <qkrwjdgus1441@naver.com>
2025-09-15 19:54:33 +0900

    refactor: 메서드 추출하여 가독성 개선

--- tyrant-client-tdd/src/test/java/tyrant/TyrantTest.java
+++ tyrant-client-tdd/src/test/java/tyrant/TyrantTest.java
@@ -16,24 +16,30 @@ class TyrantTest {
 //        t.put("key", "value");
 //        assertThat(t.get("key")).isEqualTo("value");
 
-        Socket socket = new Socket("localhost", 1978);
-        OutputStream writer = socket.getOutputStream();
-        writer.write(0xC8); // OPERATION_PREFIX
-        writer.write(0x10); // OPERATION_PUT
-        writer.write(0);
-        writer.write(0);
-        writer.write(0);
-        writer.write(3); // 4 byte key length
-        writer.write(0);
-        writer.write(0);
-        writer.write(0);
-        writer.write(5); // 4 byte value length
-        writer.write(new byte[] {'k', 'e', 'y',}); // key
-        writer.write(new byte[] {'v', 'a', 'l', 'u', 'e',}); // value
-        writer.flush();
+        new TyrantMap().put();
+    }
 
-        InputStream reader = socket.getInputStream();
-        int status = reader.read();
-        assertThat(status).isEqualTo(0);
+    private static class TyrantMap {
+        public void put() throws IOException {
+            Socket socket = new Socket("localhost", 1978);
+            OutputStream writer = socket.getOutputStream();
+            writer.write(0xC8); // OPERATION_PREFIX
+            writer.write(0x10); // OPERATION_PUT
+            writer.write(0);
+            writer.write(0);
+            writer.write(0);
+            writer.write(3); // 4 byte key length
+            writer.write(0);
+            writer.write(0);
+            writer.write(0);
+            writer.write(5); // 4 byte value length
+            writer.write(new byte[] {'k', 'e', 'y',}); // key
+            writer.write(new byte[] {'v', 'a', 'l', 'u', 'e',}); // value
+            writer.flush();
+
+            InputStream reader = socket.getInputStream();
+            int status = reader.read();
+            assertThat(status).isEqualTo(0);
+        }
     }
 }
