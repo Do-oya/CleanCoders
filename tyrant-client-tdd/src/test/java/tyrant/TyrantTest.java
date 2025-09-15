eb5cdac4486e7dbec60ce0e97f3b1ecf33ab714f
Park JeongHyun <qkrwjdgus1441@naver.com>
2025-09-15 20:15:52 +0900

    feat: tyrantMap get 메서드 구현

--- tyrant-client-tdd/src/test/java/tyrant/TyrantTest.java
+++ tyrant-client-tdd/src/test/java/tyrant/TyrantTest.java
@@ -2,9 +2,9 @@ package tyrant;
 
 import org.junit.jupiter.api.Test;
 
+import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.IOException;
-import java.io.InputStream;
 import java.net.Socket;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -19,6 +19,7 @@ class TyrantTest {
         TyrantMap tyrantMap = new TyrantMap();
         tyrantMap.open();
         tyrantMap.put("key", "value");
+        assertThat(tyrantMap.get("key")).isEqualTo("value".getBytes());
         tyrantMap.close();
     }
 
@@ -26,9 +27,10 @@ class TyrantTest {
 
         public static final int OPERATION_PREFIX = 0xC8;
         public static final int OPERATION_PUT = 0x10;
+        public static final int OPERATION_GET = 0x30;
         private Socket socket;
         private DataOutputStream writer;
-        private InputStream reader;
+        private DataInputStream reader;
 
         public void put(String key, String value) throws IOException {
             writer.write(OPERATION_PREFIX);
@@ -44,11 +46,24 @@ class TyrantTest {
         private void open() throws IOException {
             socket = new Socket("localhost", 1978);
             writer = new DataOutputStream(socket.getOutputStream());
-            reader = socket.getInputStream();
+            reader = new DataInputStream(socket.getInputStream());
         }
 
         public void close() throws IOException {
             socket.close();
+        }
+
+        public byte[] get(String key) throws IOException {
+            writer.write(OPERATION_PREFIX);
+            writer.write(OPERATION_GET);
+            writer.writeInt(key.length());
+            writer.write(key.getBytes());
+            int status = reader.read();
+            assertThat(status).isEqualTo(0);
+            int length = reader.readInt();
+            byte[] result = new byte[length];
+            reader.read(result);
+            return result;
         }
     }
 }
