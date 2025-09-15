2b00db7ce20586fb164ec6bc53acf375193bd63f
Park JeongHyun <qkrwjdgus1441@naver.com>
2025-09-15 12:36:04 +0900

    refactor: 테스트 메서드명을 더 명확하게 변경

--- tdd-practice/src/test/java/movie/MovieTest.java
+++ tdd-practice/src/test/java/movie/MovieTest.java
@@ -6,7 +6,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 class MovieTest {
     @Test
-    void canCreateMovie() {
+    void should_return_0_when_just_created() {
         Movie movie = new Movie();
         assertThat(movie.averageRating()).isEqualTo(0);
     }
