10a1cd71d5d1d9397bafc3d5738054406b3c6887
Park JeongHyun <qkrwjdgus1441@naver.com>
2025-09-16 19:39:39 +0900

    refactor: 실행되지 않는 if문 제거

--- primefactors/src/test/java/primefactors/PrimeFactorsTest.java
+++ primefactors/src/test/java/primefactors/PrimeFactorsTest.java
@@ -36,8 +36,6 @@ public class PrimeFactorsTest {
         for (;n > 1; divisor++)
             for (;n % divisor == 0; n /= divisor)
                 factors.add(divisor);
-        if (n > 1)
-            factors.add(n);
         return factors;
     }
 }
