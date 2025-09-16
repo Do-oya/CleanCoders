d714a2473833792aaa4d19b730ce87d116005069
Park JeongHyun <qkrwjdgus1441@naver.com>
2025-09-16 19:11:18 +0900

    feat: 가장 단순한 1에 대한 소인수를 구하는 테스트를 추가

--- primefactors/src/test/java/primefactors/PrimeFactorsTest.java
+++ primefactors/src/test/java/primefactors/PrimeFactorsTest.java
@@ -2,9 +2,23 @@ package primefactors;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
 public class PrimeFactorsTest {
     @Test
-    void nothing() {
-        
+    void canFactorIntoPrimes() {
+        assertThat(of(1)).isEqualTo(list());
+    }
+
+    private static List<Integer> list() {
+        return Arrays.asList();
+    }
+
+    private List<Integer> of (int n) {
+        return new ArrayList<>();
     }
 }
