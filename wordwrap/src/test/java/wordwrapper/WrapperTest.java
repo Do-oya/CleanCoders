bafcc48747cb30a61f4e554aec3d35526805569e
Park JeongHyun <qkrwjdgus1441@naver.com>
2025-09-16 20:54:37 +0900

    feat: 실패하는 테스트 추가

--- wordwrap/src/test/java/wordwrapper/WrapperTest.java
+++ wordwrap/src/test/java/wordwrapper/WrapperTest.java
@@ -13,6 +13,7 @@ public class WrapperTest {
         assertWraps("x", 1, "x");
         assertWraps("xx", 1, "x\nx");
         assertWraps("xxx", 1, "x\nx\nx");
+        assertWraps("x x", 1, "x\nx");
     }
 
     private void assertWraps(String s, int width, String expected) {
