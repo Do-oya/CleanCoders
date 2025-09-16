68e61878bb627c097f97c5250c123e8de4420166
Park JeongHyun <qkrwjdgus1441@naver.com>
2025-09-16 20:45:37 +0900

    feat: 실패하는 테스트 추가

--- wordwrap/src/test/java/wordwrapper/WrapperTest.java
+++ wordwrap/src/test/java/wordwrapper/WrapperTest.java
@@ -2,12 +2,13 @@ package wordwrapper;
 
 import org.junit.jupiter.api.Test;
 
+import static org.assertj.core.api.Assertions.*;
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class WrapperTest {
     @Test
     void should_wrap() {
-
+        assertThat(wrap(null, 1)).isEqualTo("");
     }
 
     private String wrap(String s, int width) {
