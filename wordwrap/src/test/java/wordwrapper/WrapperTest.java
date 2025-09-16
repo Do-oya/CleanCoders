101f8ebc96392c733619b28a2371562cad75d081
Park JeongHyun <qkrwjdgus1441@naver.com>
2025-09-16 20:40:42 +0900

    feat: 실패하는 테스트 추가

--- wordwrap/src/test/java/wordwrapper/WrapperTest.java
+++ wordwrap/src/test/java/wordwrapper/WrapperTest.java
@@ -9,6 +9,7 @@ public class WrapperTest {
     void should_wrap() {
         assertThat(wrap("word word", 4)).isEqualTo("word\nword");
         assertThat(wrap("a dog", 5)).isEqualTo("a dog");
+        assertThat(wrap("a dog with a bone", 6)).isEqualTo("a dog\nwith a\nbone");
     }
 
     private String wrap(String s, int width) {
