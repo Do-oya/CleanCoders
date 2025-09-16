8a5b69a1d67ecfa1bcb35c719cf26aaf54571ebe
Park JeongHyun <qkrwjdgus1441@naver.com>
2025-09-16 19:29:54 +0900

    feat: 5, 6, 7에 대한 소인수를 구하는 테스트 추가 -> 바로 통과

--- primefactors/src/test/java/primefactors/PrimeFactorsTest.java
+++ primefactors/src/test/java/primefactors/PrimeFactorsTest.java
@@ -15,6 +15,9 @@ public class PrimeFactorsTest {
         assertPrimeFactors(2, list(2));
         assertPrimeFactors(3, list(3));
         assertPrimeFactors(4, list(2, 2));
+        assertPrimeFactors(5, list(5));
+        assertPrimeFactors(6, list(2, 3));
+        assertPrimeFactors(7, list(7));
     }
 
     private void assertPrimeFactors(int n, List<Integer> list) {
