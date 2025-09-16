76ad70112450a1353361151f418a48c7185525ad
Park JeongHyun <qkrwjdgus1441@naver.com>
2025-09-16 20:49:22 +0900

    refactor: 중복 되는 코드 메서드 추출하여 가독성 개선

--- wordwrap/src/test/java/wordwrapper/WrapperTest.java
+++ wordwrap/src/test/java/wordwrapper/WrapperTest.java
@@ -8,9 +8,13 @@ import static org.assertj.core.api.Assertions.assertThat;
 public class WrapperTest {
     @Test
     void should_wrap() {
-        assertThat(wrap(null, 1)).isEqualTo("");
-        assertThat(wrap("", 1)).isEqualTo("");
-        assertThat(wrap("x", 1)).isEqualTo("x");
+        assertWraps(null, 1, "");
+        assertWraps("", 1, "");
+        assertWraps("x", 1, "x");
+    }
+
+    private void assertWraps(String s, int width, String expected) {
+        assertThat(wrap(s, width)).isEqualTo(expected);
     }
 
     private String wrap(String s, int width) {
