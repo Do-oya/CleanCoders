8da1c76f78c2ca1099c9919c2847a39c8747de3f
Park JeongHyun <qkrwjdgus1441@naver.com>
2025-09-17 15:15:52 +0900

    fitness-example (#14)
    * feat: fitness-example git clone 하여 추가
    
    * refactor: 가독성과 유지보수성 개선을 위한 내부 클래스 추출
    
    * refactor: TestableHtmlBuilder 필드 추출 및 초기화로 가독성과 유지보수성 개선
    
    * refactor: 중복된 문자열을 변수로 추출하여 가독성과 유지보수성 개선
    
    * refactor: 중복 코드 메서드로 추출하여 가독성과 유지보수성 개선
    
    * refactor: 가독성과 유지보수성 향상을 위해 반복되는 문자열을 변수로 추출
    
    * refactor: 중복된 문자열 선언 위치 수정 및 변수 재사용으로 가독성 개선
    
    * refactor: 중복 코드 includeInherited 메서드로 추출하여 가독성과 유지보수성 개선
    
    * refactor: 변수 inline으로 가독성 향상
    
    * refactor: includeSetups, includeTeardowns 메서드로 추출하여 가독성과 유지보수성 개선
    
    * refactor: 조건문 블록 추가로 가독성과 유지보수성 개선
    
    * refactor: isTestPage 메서드 추출로 가독성과 유지보수성 개선
    
    * refactor: includeSetupAndTearDowns 메서드 추출로 가독성과 유지보수성 개선
    
    * refactor: includeSetupAndTearDownsIfTestPageAndIncludeContent 메서드로 추출하여 가독성과 유지보수성 개선
    
    * chore: VCS 매핑에 fitness-example 디렉토리 추가 및 미사용 빌드 파일 정리

---
+++ fitness-example/src/main/java/function/FitnessExample.java
@@ -0,0 +1,70 @@
+package function;
+
+import fitnesse.responders.run.SuiteResponder;
+import fitnesse.wiki.*;
+
+public class FitnessExample {
+    public String testableHtml(PageData pageData, boolean includeSuiteSetup) throws Exception {
+        return new TestableHtmlBuilder(pageData, includeSuiteSetup).invoke();
+    }
+
+    private class TestableHtmlBuilder {
+        private final StringBuffer buffer;
+        private PageData pageData;
+        private boolean includeSuiteSetup;
+        private WikiPage wikiPage;
+
+        public TestableHtmlBuilder(PageData pageData, boolean includeSuiteSetup) {
+            this.pageData = pageData;
+            wikiPage = pageData.getWikiPage();
+            this.includeSuiteSetup = includeSuiteSetup;
+            buffer = new StringBuffer();
+        }
+
+        public String invoke() throws Exception {
+            includeSetupAndTearDownsIfTestPageAndIncludeContent();
+            return pageData.getHtml();
+        }
+
+        private void includeSetupAndTearDownsIfTestPageAndIncludeContent() throws Exception {
+            if (isTestPage())
+                includeSetupAndTearDowns();
+            pageData.setContent(buffer.toString());
+        }
+
+        private void includeSetupAndTearDowns() throws Exception {
+            includeSetups();
+            buffer.append(pageData.getContent());
+            includeTeardowns();
+        }
+
+        private boolean isTestPage() throws Exception {
+            return pageData.hasAttribute("Test");
+        }
+
+        private void includeTeardowns() throws Exception {
+            includeInherited("TearDown", "teardown");
+            if (includeSuiteSetup)
+                includeInherited(SuiteResponder.SUITE_TEARDOWN_NAME, "teardown");
+        }
+
+        private void includeSetups() throws Exception {
+            if (includeSuiteSetup)
+                includeInherited(SuiteResponder.SUITE_SETUP_NAME, "setup");
+            includeInherited("SetUp", "setup");
+        }
+
+        private void includeInherited(String pageName, String mode) throws Exception {
+            WikiPage suiteSetup = PageCrawlerImpl.getInheritedPage(pageName, wikiPage);
+            if (suiteSetup != null) {
+                includePage(suiteSetup, mode);
+            }
+        }
+
+        private void includePage(WikiPage suiteSetup, String mode) throws Exception {
+            WikiPagePath pagePath = wikiPage.getPageCrawler().getFullPath(suiteSetup);
+            String pagePathName = PathParser.render(pagePath);
+            buffer.append("!include -" + mode + " .").append(pagePathName).append("\n");
+        }
+    }
+}
